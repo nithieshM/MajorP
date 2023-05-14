05e1471b56ac5ec18fd2be9688700b429cf88ae3
Nithiesh Mohan <60309668+nithieshM@users.noreply.github.com>
2023-05-14 11:39:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,20 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Conv1D
 from sklearn.metrics import r2_score
 import seaborn as sns
+import plotly.express as px
+import requests
+from bs4 import BeautifulSoup
+import json
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# NLTK VADER for sentiment analysis
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# for extracting data from finviz
+finviz_url = 'https://finviz.com/quote.ashx?t='
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -83,6 +97,82 @@ def decision_tree_app():
     ax.plot(valid[['Close', 'Predictions']])
     ax.legend(["Original", "Valid", "Predicted"])
     st.pyplot(fig)
+
+    def get_news(ticker):
+        url = finviz_url + ticker
+        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'})
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        news_table = soup.find(id='news-table')
+        return news_table
+
+    def parse_news(news_table):
+        parsed_news = []
+
+        for x in news_table.findAll('tr'):
+            text = x.a.get_text()
+            date_scrape = x.td.text.split()
+
+            if len(date_scrape) == 1:
+                time = date_scrape[0]
+            else:
+                date = date_scrape[0]
+                time = date_scrape[1]
+
+            parsed_news.append([date, time, text])
+
+        columns = ['date', 'time', 'headline']
+        parsed_news_df = pd.DataFrame(parsed_news, columns=columns)
+        parsed_news_df['datetime'] = pd.to_datetime(parsed_news_df['date'] + ' ' + parsed_news_df['time'])
+
+        return parsed_news_df
+
+    def score_news(parsed_news_df):
+        vader = SentimentIntensityAnalyzer()
+        scores = parsed_news_df['headline'].apply(vader.polarity_scores).tolist()
+        scores_df = pd.DataFrame(scores)
+        parsed_and_scored_news = parsed_news_df.join(scores_df, rsuffix='_right')
+        parsed_and_scored_news = parsed_and_scored_news.set_index('datetime')
+        parsed_and_scored_news = parsed_and_scored_news.drop(['date', 'time'], 1)
+        parsed_and_scored_news = parsed_and_scored_news.rename(columns={"compound": "sentiment_score"})
+
+        return parsed_and_scored_news
+
+    def plot_hourly_sentiment(parsed_and_scored_news, ticker):
+        mean_scores = parsed_and_scored_news.resample('H').mean()
+        fig = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title=ticker + ' Hourly Sentiment Scores')
+        return fig
+
+    def plot_daily_sentiment(parsed_and_scored_news, ticker):
+        mean_scores = parsed_and_scored_news.resample('D').mean()
+        fig = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title=ticker + ' Daily Sentiment Scores')
+        return fig
+
+    def xdd():
+        
+        ticker = st.sidebar.text_input("Enter stock ticker (e.g. AAPL for Apple)", "AAPL")
+        
+        news_table = get_news(ticker)
+        
+        parsed_news_df = parse_news(news_table)
+        parsed_and_scored_news = score_news(parsed_news_df)
+        fig_hourly = plot_hourly_sentiment(parsed_and_scored_news, ticker)
+        fig_daily = plot_daily_sentiment(parsed_and_scored_news, ticker)
+
+        st.header("Hourly and Daily Sentment of {} Stock".format(ticker))
+        st.plotly_chart(fig_hourly, use_container_width=True)
+        st.plotly_chart(fig_daily, use_container_width=True)
+
+        st.subheader("Recent Headlines")
+        st.dataframe(parsed_and_scored_news[['headline', 'neg', 'neu', 'pos', 'sentiment_score']].head())
+
+        st.markdown("""
+            *The above charts show the average sentiment scores of {} stock on an hourly and daily basis.*
+            *The table displays the most recent headlines of the stock along with their negative, neutral, positive, and aggregated sentiment scores.*
+            *The news headlines are obtained from the FinViz website.*
+            *Sentiments are given by the nltk.sentiment.vader Python library.*
+        """.format(ticker))
+
    
     
 
